ab0d15082da6aabb4bb1efa3a592267fdf472edd
billchheu <tuh04091@temple.edu>
2020-07-21 16:18:45 -0500

    added way to get random japanese word from jisho.org

--- scrape.js
+++ scrape.js
@@ -10,6 +10,8 @@ function scrapeSite(url) {
            const furigana = $(".furigana").first().text();
            const definition = $(".meaning-meaning").html();
             // TODO: ADD SENTENCE?
+
+            // TODO: OUTPUT TO FILE
            console.log(japanese, furigana, definition);
         }
         
@@ -17,17 +19,29 @@ function scrapeSite(url) {
     });
 }
 
-function getRandomWord() {
+function getRandomWord(callback) {
     let randomWord = Math.floor((Math.random() * 21073) + 1);
     // 20 entries per page
     let page = randomWord / 20;
     let wordOnPage = randomWord % 20;
 
-    let url = "https://jisho.org/search/%23common%20%23words?page=" +page;
+    let url = "https://jisho.org/search/%23common%23words?page=" + page;
 
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
 
+            let randomJapaneseLink = $(".light-details_link").eq(wordOnPage).attr("href");
+
+            // addes https to the link so that the request can handle the link
+            randomJapaneseLink = "https:" + randomJapaneseLink;
+            console.log(randomJapaneseLink);
+            callback(randomJapaneseLink);
+
+        } 
+    });
 }
 
+getRandomWord(scrapeSite);
 
-
-scrapeSite("https://jisho.org/word/%E5%B7%9D")
+//scrapeSite();
