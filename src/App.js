897bce49cfbf8ed32a7c162fe78f0f7e13dd9dd7
BillChheu <tuh04091@temple.edu>
2020-07-30 14:19:24 -0400

    added react and formatting of text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import randomWord from './app/randomWord.json'
 import './App.css';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+         <h1 className = "japanese">{randomWord.word}</h1>
+         <h2 className = "definition">{randomWord.definition}</h2>
+         <a href = {randomWord.url} className = "link">Source</a>
+      
     </div>
   );
+
 }
+
 
 export default App;
---
+++ src/app/scrape.js
@@ -0,0 +1,62 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+function scrapeSite(url) {
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
+
+           const japanese = $(".text").last().text().replace(/\s+/g,'');
+           const furigana = $(".furigana").first().text().replace(/\s+/g,'');
+           const definition = $(".meaning-meaning").html();
+            // TODO: ADD SENTENCE?
+
+            const japaneseInfo = {
+                word: japanese,
+                furigana: furigana,
+                definition: definition,
+                url: url
+            }
+
+            const jsonString = JSON.stringify(japaneseInfo, null, 2);
+            fs.writeFile("randomWord.json", jsonString, (error) => {
+                if (error) {
+                    console.log("Error writing file", error);
+                } else {
+                    console.log("Successfully wrote file")
+                }
+            });
+
+
+           console.log(japanese, furigana, definition);
+        }
+        
+    
+    });
+}
+
+function getRandomWord(callback) {
+    let randomWord = Math.floor((Math.random() * 21073) + 1);
+    // 20 entries per page
+    let page = randomWord / 20;
+    let wordOnPage = randomWord % 20;
+
+    let url = "https://jisho.org/search/%23common%23words?page=" + page;
+
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
+
+            let randomJapaneseLink = $(".light-details_link").eq(wordOnPage).attr("href");
+
+            // addes https to the link so that the request can handle the link
+            randomJapaneseLink = "https:" + randomJapaneseLink;
+            console.log(randomJapaneseLink);
+            callback(randomJapaneseLink);
+
+        } 
+    });
+}
+
+getRandomWord(scrapeSite);
